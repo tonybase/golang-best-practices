7b134f00ca6fde258a642517cd65982b16d9efb9
chenzhihui <zhihui_chen@foxmail.com>
2020-12-06 00:47:41 +0800

    add version info

--- build-version/main.go
+++ build-version/main.go
@@ -1,22 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// go build -ldflags "-X main.Version=x.y.yz"
-var (
-	// Version is the version of the compiled software.
-	Version string
-	// Branch is current branch name the code is built off
-	Branch string
-	// Revision is the short commit hash of source tree
-	Revision string
-	// BuildDate is the date when the binary was built.
-	BuildDate string
+	"github.com/tonybase/golang-best-practices/build-version/version"
 )
 
 func main() {
-	fmt.Println("version:", Version)
-	fmt.Println("branch:", Branch)
-	fmt.Println("revision:", Revision)
-	fmt.Println("build_date:", BuildDate)
+	fmt.Println("version:", version.Version)
+	fmt.Println("branch:", version.Branch)
+	fmt.Println("revision:", version.Revision)
+	fmt.Println("build_date:", version.BuildDate)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tonybase/golang-best-practices
+
+go 1.13
